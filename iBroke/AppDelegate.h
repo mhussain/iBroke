6a398ccfd9a470645be9b6464cb3a166cb92b246
Mujtaba Hussain <mujtaba.hussain@rea-group.com>
2011-10-19 12:56:27 +1100

    Initial Commit

---
+++ iBroke/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBroke
+//
+//  Created by Mujtaba Hussain on 19/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
