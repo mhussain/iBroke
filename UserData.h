f2ee12127b751ec960e7049d3d548683c902d13e
Mujtaba Hussain <mujtaba.hussain@rea-group.com>
2011-10-27 17:00:43 +1100

    Added Third party again

---
+++ UserData.h
@@ -0,0 +1,13 @@
+//
+//  UserData.h
+//  iBroke
+//
+//  Created by Mujtaba Hussain on 27/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserData : NSObject
+
+@end
---
+++ UserData.m
@@ -0,0 +1,13 @@
+//
+//  UserData.m
+//  iBroke
+//
+//  Created by Mujtaba Hussain on 27/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserData.h"
+
+@implementation UserData
+
+@end
