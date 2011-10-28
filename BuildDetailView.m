35d5c4503d2eaca87f2891c4b938808e1dcadf67
Michael Rowe <mrowe@mojain.com>
2011-10-28 14:55:06 +1100

    Don't do that shit in drawRect

--- BuildDetailView.m
+++ BuildDetailView.m
@@ -11,35 +11,23 @@
 
 @implementation BuildDetailView
 
-@synthesize build;
-
-- (id)initWithFrame:(CGRect)frame;
+- (id)initWithFrame:(CGRect)frame build:(Build *)build;
 {
   if ((self = [super initWithFrame:frame])) {
     [self setBackgroundColor:[UIColor whiteColor]];
+
+    UILabel *build_name = [[UILabel alloc] initWithFrame:CGRectMake(0., 10., CGRectGetMaxX(frame), 40.)];
+    [build_name setTextAlignment:UITextAlignmentCenter];
+    [build_name setFont:[UIFont boldSystemFontOfSize:22.]];
+    [build_name setText:[build name]];
+    [self addSubview:build_name];
+
+    UILabel *build_status = [[UILabel alloc] initWithFrame:CGRectMake(0., 50., CGRectGetMaxX(frame), 40.)];
+    [build_status setTextAlignment:UITextAlignmentCenter];
+    [build_status setText:[NSString stringWithFormat:@"Status: %@", [build status]]];
+    [self addSubview:build_status];
   }
   return self;
-}
-
-
-#pragma mark - UIView Lifecycle
-
-- (void)drawRect:(CGRect)rect;
-{
-  UILabel *build_name = [[UILabel alloc] initWithFrame:CGRectMake(0., 10., CGRectGetMaxX(rect), 40.)];
-  [build_name setTextAlignment:UITextAlignmentCenter];
-  [build_name setFont:[UIFont boldSystemFontOfSize:22.]];
-  [build_name setText:[build name]];
-  [self addSubview:build_name];
-
-  UILabel *build_status = [[UILabel alloc] initWithFrame:CGRectMake(0., 50., CGRectGetMaxX(rect), 40.)];
-  [build_status setTextAlignment:UITextAlignmentCenter];
-  [build_status setText:[NSString stringWithFormat:@"Status: %@", [build status]]];
-  [self addSubview:build_status];
-}
-
-- (void)refresh;
-{
 }
 
 @end
